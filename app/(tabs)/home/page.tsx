c6e6ff27be30f9b9285b949be37dbfed6fe3b21c
amelia <amelia@dunamu.com>
2024-05-16 20:43:48 +0900

    modal

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -1,7 +1,9 @@
 import ListProduct from "@/components/list-product";
 import ProductList from "@/components/product-list";
 import db from "@/lib/db";
+import { PlusIcon } from "@heroicons/react/24/solid";
 import type { Prisma } from "@prisma/client";
+import Link from "next/link";
 
 async function getInitialProducts() {
   const response = await db.product.findMany({
@@ -24,11 +26,17 @@ export type InitialProducts = Prisma.PromiseReturnType<
   typeof getInitialProducts
 >;
 
-export default async function Product() {
+export default async function Products() {
   const initialProducts = await getInitialProducts();
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <Link
+        href="/products/add"
+        className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
+      >
+        <PlusIcon className="size-10" />
+      </Link>
     </div>
   );
 }
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { getMoreProducts } from "@/app/(tabs)/products/actions";
+import { getMoreProducts } from "@/app/(tabs)/home/actions";
 import ListProduct from "@/components/list-product";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface ProductListProps {
   initialProducts: {
@@ -15,24 +15,60 @@ interface ProductListProps {
 }
 
 export default function ProductList({ initialProducts }: ProductListProps) {
-  const [products, setproducts] = useState(initialProducts);
+  const [products, setProducts] = useState(initialProducts);
 
   const [isLoading, setIsLoading] = useState(false);
-  const onLoadMoreClick = async () => {
-    setIsLoading(true);
-    const newProducts = await getMoreProducts(1);
-    setproducts((prev) => [...prev, ...newProducts]);
-    setIsLoading(false);
-  };
+  const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(true);
+
+  const trigger = useRef<HTMLSpanElement>(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async (
+        entries: IntersectionObserverEntry[],
+        observer: IntersectionObserver,
+      ) => {
+        const element = entries[0];
+        if (element.isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          setIsLoading(true);
+          const newProducts = await getMoreProducts(page + 1);
+          if (newProducts.length !== 0) {
+            setPage((prev) => prev + 1);
+            setProducts((prev) => [...prev, ...newProducts]);
+          } else {
+            setIsLastPage(true);
+          }
+          setIsLoading(false);
+        }
+      },
+      {
+        threshold: 1.0,
+      },
+    );
+    if (trigger.current) {
+      observer.observe(trigger.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, [page]);
 
   return (
     <div className="p-5 flex flex-col gap-5">
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      <button onClick={onLoadMoreClick} disabled={isLoading}>
+
+      <span
+        ref={trigger}
+        style={{
+          marginTop: `${page * 300}vh`,
+        }}
+        className="mt-[300vh] mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+      >
         {isLoading ? "Loading..." : "Load More"}
-      </button>
+      </span>
     </div>
   );
 }
